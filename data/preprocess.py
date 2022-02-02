9f15d2f05ed19ce834075f9ed570a7c568dd3558
masteroppgave2022 <masteroppgave.2022@gmail.com>
2022-02-02 11:47:28 +0000

    finished tutorial

---
+++ data/preprocess.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt 
+import matplotlib.colors as colors i
+mport os
+import snappy
+from snappy import Product
+from snappy import ProductIO 
+from snappy import ProductUtils 
+from snappy import WKTReader 
+from snappy import HashMap
+from snappy import GPF
+
+# For shapefiles
+import shapefile 
+import pygeoif
