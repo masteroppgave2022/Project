e290165d26345681d72b6d0f23a9d8a4a8c6fd3f
masteroppgave2022 <masteroppgave.2022@gmail.com>
2022-02-04 12:06:27 +0000

    restructure

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import data.request as req
 # import data.preprocess as pp
 
 if __name__ == '__main__':
-    download = req.requestDownload(username='mikaelvagen',password='Masteroppgave2022',\
-        search_config='data/search_configs/gaula_kvål_06_2021.cfg')
+    # download = req.requestDownload(username='mikaelvagen',password='Masteroppgave2022',\
+    #    search_config='data/search_configs/gaula_kvål_06_2021.cfg')
+    pass
