111c4cb13c052d2139688aef8d121fab9a22bcac
masteroppgave2022 <masteroppgave.2022@gmail.com>
2022-02-04 13:44:29 +0000

    python package install script

---
+++ setup/py_setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+PACKAGES=(
+    "numpy" 
+    "matplotlib" 
+    "tensorflow==2.7.0" 
+    "keras" "pandas" 
+    "segmentation_models" 
+    "seaborn" 
+    "opencv-python" 
+    "scikit-learn" 
+    "datetime" 
+    "sentinelsat" 
+    "asf_search" 
+    "geopandas" 
+    "pyshp" 
+    "shapely")
+
+PREFIX="python -m pip install "
+
+for package in ${PACKAGES[@]}; do
+	COMMAND=$PREFIX$package
+	eval $COMMAND
+done
