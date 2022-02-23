6439b68a06cf9fac31c98c1a2209d88cd337989b
masteroppgave2022 <masteroppgave.2022@gmail.com>
2022-02-23 20:34:22 +0000

    label preprocessing complete

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     logging.basicConfig(filename='main_log.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s',  datefmt='%m/%d/%Y %H:%M:%S')
     parser_main = configparser.ConfigParser()
     parser_main.read('/localhome/studenter/mikaellv/Project/main_config.ini')
-
+    root = parser_main['main']['root']
     download_path = parser_main['main']['download_path']
     shapefile_path = parser_main['main']['shapefile_path']
     subset_path = parser_main['main']['subset_path']
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     if parser_main.getboolean('main','download'): # Download True/False
         user = parser_main['download']['username']
         pw = parser_main['download']['password']
-        search_configs = 'data/search_configs/'
+        search_configs = root+'search_configs/'
         parser_locations = configparser.ConfigParser()
         parser_locations.read(search_configs+'LOCATIONS.ini')
         configs = []
@@ -61,21 +61,20 @@ if __name__ == '__main__':
 
     if parser_main.getboolean('main', 'preprocess'):
         """ Processing pipeline to be implemented here """
-        print("[INFO] processing...")
+        print("[INFO] Processing...")
         pp = Preprocess()
 
-        
         if parser_main.getboolean('preprocess', 'clip_shapefile'):
-            print('[INFO] Clipping shapefile ... ')
-            src = '/localhome/studenter/mikaellv/Project/data/FBK_vann/FKB_vann.shp'
-            dest = '/localhome/studenter/mikaellv/Project/data/untiled_masks/'
-            extent_root = '/localhome/studenter/mikaellv/Project/data/shapefiles/'
+            print('[INFO] Clipping label shapefiles ... ')
+            src = root + 'FKB_vann/FKB_vann.shp'
+            dest = root + 'untiled_masks/'
+            extent_root = shapefile_path
             locations = os.listdir(extent_root)
             locations.remove('.DS_Store')
             extents = [extent_root+f+'/'+f+'.shp' for f in locations]
             pp.clip_shapefile(src,extents,dest)
-            print('[INFO] Done.')
-         
+            print('[INFO] Done with all!')
+        
         for file in os.listdir(download_path):
             if file.startswith("."): continue
             if file.startswith("S1A_IW_GRDH_1SDV"): continue
@@ -114,6 +113,7 @@ if __name__ == '__main__':
                     logging.info(f"Terrain correction for {subset} finished")
                     pp.save_product(subset_O_TNR_C_TC, 'test', save_path)
                     logging.info(f"Subset {subset} preprocessed and saved")
+                    print("Finished.")
 
 
 
