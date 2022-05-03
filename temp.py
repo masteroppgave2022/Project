b2c6307f47e564ec007cdc816ffadd0afc2d6091
masteroppgave2022 <masteroppgave.2022@gmail.com>
2022-05-03 11:13:17 +0000

    dataset analysis

--- temp.py
+++ temp.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = '/localhome/studenter/mikaellv/Project/ML/saved_dataframes/deeplab_test_long_2.csv.csv'
+file_path = '/localhome/studenter/renatask/Project/ML/saved_dataframes/DeepLabV3_all_epochs.csv'
 
 file = pd.read_csv(file_path)
 
 print(file)
 
-val_loss_fig = file.loc[:, ['loss', 'val_loss']].plot()
-crossentropy_fig = file.loc[:, ['categorical_crossentropy', 'val_categorical_crossentropy']].plot()
+val_loss_fig = file.loc[:, ['loss', 'val_loss']].plot(title='DeepLabV3+', xlabel='Epoch', ylabel='DICE Loss')
+crossentropy_fig = file.loc[:, ['categorical_crossentropy', 'val_categorical_crossentropy']].plot(title='DeepLabV3+', xlabel='Epoch', ylabel='Categorical Cross-Entropy')
 # dice_fig = file.loc[:, ['dice_loss', 'val_dice_loss']].plot()
-val_acc_fig = file.loc[:, ['acc', 'val_acc']].plot()
-# loss_fig = file.loc[:, ['dice_loss', 'val_dice_loss', 'categorical_crossentropy', 'val_categorical_crossentropy']].plot()
+val_acc_fig = file.loc[:, ['acc', 'val_acc']].plot(title='DeepLabV3+', xlabel='Epoch', ylabel='Accuracy')
+loss_fig = file.loc[:, ['loss', 'val_loss', 'categorical_crossentropy', 'val_categorical_crossentropy']].plot(title='DeepLabV3+', xlabel='Epoch', ylabel='Loss')
 plt.show()
